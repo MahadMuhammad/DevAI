084d364799129ebbff84c55ec12a9ae822a96b34
AbdulMoiz03 <abdulmoizliaqat@gmail.com>
2025-05-07 00:32:02 +0500

    Enhance README and launch configuration; add inline completion trigger command and improve API handling

--- playground.py
+++ playground.py
@@ -1,2 +1,7 @@
-def isEven(n: int):
-    if n % 2 ==    
+def CalculateBMI(n1, n2):
+    return (n1 / (n2 * n2))
+
+height = float(input("Enter your height in cm: "))
+weight = float(input("Enter your weight in kg: "))
+bmi = CalculateBMI(weight, height**2)
+print("Your BMI is:", bmi)
